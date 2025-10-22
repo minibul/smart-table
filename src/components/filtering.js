4a99da2854807eb6b2b3488ba487aa9057096312
a.bondarev <a.bondarev@agcsoft.com>
2025-10-22 09:12:26 +0300

    refactoring

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,37 +1,48 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+export function initFiltering(elements) {
+  const updateIndexes = (elements, indexes) => {
+    Object.keys(indexes).forEach((elementName) => {
+      elements[elementName].append(
+        ...Object.values(indexes[elementName]).map((name) => {
+          const option = document.createElement("option");
+          option.textContent = name;
+          option.value = name;
+          return option;
+        })
+      );
+    });
+  };
 
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
-
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)
-        .forEach((elementName) => {
-            elements[elementName].append(
-                ...Object.values(indexes[elementName])
-                    .map(name => {
-                        const option = document.createElement('option');
-                        option.value = name;
-                        option.textContent = name;
-                        return option;
-                    })
-            )
-        });
-
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-        if (action && action.name === 'clear') {
-            const input = action.parentElement.querySelector('input');
-            const field = action.dataset.field;
-            if (input) {
-                input.value = '';
-            }
-            if (field && state[field] !== undefined) {
-                state[field] = '';
-            }
-        }
-
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
+  const applyFiltering = (query, state, action) => {
+    if (action && action.name === "clear") {
+      const input = action.parentElement.querySelector("input");
+      const field = action.dataset.field;
+      if (input) {
+        input.value = "";
+      }
+      if (field && state[field] !== undefined) {
+        state[field] = "";
+      }
     }
+
+    const filter = {};
+    Object.keys(elements).forEach((key) => {
+      if (elements[key]) {
+        if (
+          ["INPUT", "SELECT"].includes(elements[key].tagName) &&
+          elements[key].value
+        ) {
+          filter[`filter[${elements[key].name}]`] = elements[key].value;
+        }
+      }
+    });
+
+    return Object.keys(filter).length
+      ? Object.assign({}, query, filter)
+      : query;
+  };
+
+  return {
+    updateIndexes,
+    applyFiltering,
+  };
 }
--- src/components/table.js
+++ src/components/table.js
@@ -1,63 +1,53 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from "../lib/utils.js";
 
-/**
- * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
- *
- * @param {Object} settings
- * @param {(action: HTMLButtonElement | undefined) => void} onAction
- * @returns {{container: Node, elements: *, render: render}}
- */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
+  const { tableTemplate, rowTemplate, before, after } = settings;
+  const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
-    before.reverse().forEach(subName => {
-        root[subName] = cloneTemplate(subName);
-        root.container.prepend(root[subName].container);
+  before.reverse().forEach((subName) => {
+    root[subName] = cloneTemplate(subName);
+    root.container.prepend(root[subName].container);
+  });
+
+  after.forEach((subName) => {
+    root[subName] = cloneTemplate(subName);
+    root.container.append(root[subName].container);
+  });
+
+  root.container.addEventListener("change", () => {
+    onAction();
+  });
+
+  root.container.addEventListener("reset", () => {
+    setTimeout(onAction);
+  });
+
+  root.container.addEventListener("submit", (event) => {
+    event.preventDefault();
+    onAction(event.submitter);
+  });
+
+  const render = (data) => {
+    const nextRows = data.map((item) => {
+      const row = cloneTemplate(rowTemplate);
+
+      Object.keys(item).forEach((key) => {
+        if (key in row.elements) {
+          const element = row.elements[key];
+          const tagName = element.tagName.toLowerCase();
+
+          if (tagName === "input" || tagName === "select") {
+            element.value = item[key];
+          } else {
+            element.textContent = item[key];
+          }
+        }
+      });
+
+      return row.container;
     });
+    root.elements.rows.replaceChildren(...nextRows);
+  };
 
-    after.forEach(subName => {
-        root[subName] = cloneTemplate(subName);
-        root.container.append(root[subName].container);
-    });
-
-    // @todo: #1.3 —  обработать события и вызвать onAction()
-    root.container.addEventListener('change', () => {
-        onAction();
-    });
-
-    root.container.addEventListener('reset', () => {
-        setTimeout(onAction);
-    });
-
-    root.container.addEventListener('submit', (e) => {
-        e.preventDefault();
-        onAction(e.submitter);
-    });
-
-    const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = data.map(item => {
-            const row = cloneTemplate(rowTemplate);
-            
-            Object.keys(item).forEach(key => {
-                if (key in row.elements) {
-                    const element = row.elements[key];
-                    const tagName = element.tagName.toLowerCase();
-                    
-                    if (tagName === 'input' || tagName === 'select') {
-                        element.value = item[key];
-                    } else {
-                        element.textContent = item[key];
-                    }
-                }
-            });
-            
-            return row.container;
-        });
-        root.elements.rows.replaceChildren(...nextRows);
-    }
-
-    return {...root, render};
+  return { ...root, render };
 }
